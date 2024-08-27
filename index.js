28f08f7d66e171b5c10b82e83fc62ab7346ef089
Yatika Jena <j.yatika@iitg.ac.in>
2024-08-27 20:07:53 +0530

    woahh

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express= require("express")
 const app= express()
 const mongoose= require("mongoose")
+const Product= require("./models/product.model")
 
+app.use(express.json())
 
+app.get('/',(req,res)=>{
+    res.send("hii")
+})
+app.get('/api/products',async(req,res)=>{
+    try{
+        const products= await Product.find({})
+        res.status(200).json(products)
+    }catch(e){
+        res.status(500).json({message: e.message})
+    }
+})
 
+app.post('/api/products',async(req,res)=>{
+    try{
+        const product= await Product.create(req.body)
+        res.status(200).json(product)
+    }catch(e){
+        res.status(500).json({message: e.message})
+    }
+})
 
 mongoose.connect("mongodb+srv://jenayatika:xPw0AlpDnykkkglK@backenddb.2e5sb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backendDB")
 .then(()=>{
--- models/product.model.js
+++ models/product.model.js
@@ -1,1 +1,27 @@
-const mongoose= require("mongoose")
+const mongoose= require("mongoose");
+
+const productSchema= mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: [true, "please enter product name"]
+        },
+        quantity: {
+            type: Number,
+            required:true,
+            default:0
+        },
+        image:{
+            type: String,
+            required: false
+        },
+
+    },
+    {
+        timestamps: true
+    }
+
+)
+const Product= mongoose.model("product",productSchema);
+
+module.exports= Product
